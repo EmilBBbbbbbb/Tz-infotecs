0e51411e60a94b06cd3d6b72f22a66eb73886386
EmilBBbbbbbb <emilianevgenevishborisov@gmail.com>
2025-02-04 17:05:48 +0300

    Finish First task

--- main.py
+++ main.py
@@ -7,42 +7,38 @@ import requests_cache
 import pandas as pd
 from retry_requests import retry
 
-
-# Setup the Open-Meteo API client with cache and retry on error
-cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
-retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-openmeteo = openmeteo_requests.Client(session = retry_session)
+# Set up the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
+retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+open_meteo = openmeteo_requests.Client(session=retry_session)
 
 url = "https://api.open-meteo.com/v1/forecast"
 
-
 engine = create_async_engine('sqlite+aiosqlite:///weather.db')
-
 
 app = FastAPI()
 
 
-@app.get('/weather/{latitude,longitude}')
-def get_weather_now(latitude:float,longitude:float):
-	params = {
-		"latitude": latitude,
-		"longitude": longitude,
-		"current": ["temperature_2m", "surface_pressure", "wind_speed_10m"],
-		"forecast_days": 1
-	}
-	responses = openmeteo.weather_api(url, params=params)
-	response = responses[0]
+@app.get('/weather/current')
+def get_weather_now(latitude: float, longitude: float):
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "current": ["temperature_2m", "surface_pressure", "wind_speed_10m"],
+        "forecast_days": 1
+    }
+    responses = open_meteo.weather_api(url, params=params)
+    response = responses[0]
 
-	current = response.Current()
-	current_temperature_2m = current.Variables(0).Value()
-	current_surface_pressure = current.Variables(1).Value()
-	current_wind_speed_10m = current.Variables(2).Value()
+    current = response.Current()
+    current_temperature_2m = current.Variables(0).Value()
+    current_surface_pressure = current.Variables(1).Value()
+    current_wind_speed_10m = current.Variables(2).Value()
 
-	return {"Current temperature_2m":current_temperature_2m,
-			"Current surface_pressure":current_surface_pressure,
-			"Current wind_speed_10m":current_wind_speed_10m}
-
+    return {"Current temperature_2m": current_temperature_2m,
+            "Current surface_pressure": current_surface_pressure,
+            "Current wind_speed_10m": current_wind_speed_10m}
 
 
 if __name__ == "__main__":
-	uvicorn.run('main:app', reload=True)
+    uvicorn.run('main:app', reload=True)
